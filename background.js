71e7820915840e1bd75fdbab67948bcc861106f1
tiffaina <98503566+tiffaina@users.noreply.github.com>
2024-06-06 21:30:49 -0500

    gpt3

--- background.js
+++ background.js
@@ -1,17 +1,16 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Tab Search extension installed.');
-  });
-  
-  chrome.action.onClicked.addListener((tab) => {
+});
+
+chrome.action.onClicked.addListener(() => {
     console.log('Extension icon clicked');
     chrome.tabs.query({}, function(tabs) {
-      if (chrome.runtime.lastError) {
-        console.error('Error querying tabs:', chrome.runtime.lastError);
-        return;
-      }
-      chrome.storage.local.set({ allTabs: tabs }, () => {
-        console.log('All tabs stored:', tabs);
-      });
+        if (chrome.runtime.lastError) {
+            console.error('Error querying tabs:', chrome.runtime.lastError);
+            return;
+        }
+        chrome.storage.local.set({ allTabs: tabs }, () => {
+            console.log('All tabs stored:', tabs);
+        });
     });
-  });
-  
+});
